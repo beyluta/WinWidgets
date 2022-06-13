5fe0a04bec5b2c6deacda7fe07048958291e6823
Beyluta <beyluta1@gmail.com>
2022-06-13 21:44:15 +0200

    remove unused code

--- Classes/WindowEssentials.cs
+++ Classes/WindowEssentials.cs
@@ -4,20 +4,12 @@ using System.Windows;
 
 namespace Widgets
 {
-    class WindowEssentials
+    internal class WindowEssentials
     {
-        public const int SW_MAXIMIZE = 3;
-        public const int SW_MINIMIZE = 6;
         public const int SW_HIDE = 0;
-        public const UInt32 WM_CLOSE = 0x0010;
         public const int GWL_EXSTYLE = -20;
         public const int WS_EX_LAYERED = 0x80000;
         public const int LWA_ALPHA = 0x2;
-        public const int LWA_COLORKEY = 0x1;
-        public const short SWP_NOMOVE = 0X2;
-        public const short SWP_NOSIZE = 1;
-        public const short SWP_NOZORDER = 0X4;
-        public const int SWP_SHOWWINDOW = 0x0040;
 
         [StructLayout(LayoutKind.Sequential)]
         public struct POINT
@@ -30,12 +22,6 @@ namespace Widgets
                 return new Point(point.X, point.Y);
             }
         }
-
-        [DllImport("user32.dll", CharSet = CharSet.Auto)]
-        public static extern IntPtr SendMessage(IntPtr hWnd, UInt32 Msg, IntPtr wParam, IntPtr lParam);
-
-        [DllImport("user32.dll", EntryPoint = "FindWindow")]
-        public static extern IntPtr FindWindowByCaption(IntPtr ZeroOnly, string lpWindowName);
 
         [DllImport("user32.dll")]
         [return: MarshalAs(UnmanagedType.Bool)]
@@ -66,8 +52,5 @@ namespace Widgets
 
         [DllImport("user32.dll")]
         public static extern bool SetLayeredWindowAttributes(IntPtr hwnd, uint crKey, byte bAlpha, uint dwFlags);
-
-        [DllImport("user32.dll", EntryPoint = "SetWindowPos")]
-        public static extern IntPtr SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int x, int Y, int cx, int cy, int wFlags);
     }
 }
