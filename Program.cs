6026bded4c03c597d1607dbfd1bfdc4dcf506a2a
Beyluta <beyluta1@gmail.com>
2022-11-26 13:23:46 +0100

    Fix critical bugs that affect stability
    Fix a bug with the new Windows 11 22H2 Update and fix an oversight that caused widgets to crash the widget manager.

--- Program.cs
+++ Program.cs
@@ -8,8 +8,6 @@ internal class Program : WindowEssentials
     [STAThread]
     private static void Main(string[] args)
     {
-        ShowWindow(GetConsoleWindow(), SW_HIDE);
-
         bool processExists = System.Diagnostics.Process.GetProcessesByName(
             System.IO.Path.GetFileNameWithoutExtension(
                 System.Reflection.Assembly.GetEntryAssembly().Location))
