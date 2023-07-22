1d03b1254c54508b1baf6d238126a3c7a633b5b0
Beyluta <beyluta1@gmail.com>
2023-07-22 16:27:19 +0200

    remove old form class

--- Services/Timer.Service.cs
+++ Services/Timer.Service.cs
@@ -4,12 +4,21 @@ namespace Services
 {
     internal class TimerService
     {
-        public void CreateTimer(int miliseconds, ElapsedEventHandler elapsedEventHandler, bool autoReset, bool enabled)
+        /// <summary>
+        /// Creates and automatically starts a timer
+        /// </summary>
+        /// <param name="milliseconds">milliseconds until the next tick</param>
+        /// <param name="elapsedEventHandler">Event handler that will be called every tick</param>
+        /// <param name="autoReset">should timer be automatically reset</param>
+        /// <param name="enabled">should timer be enabled</param>
+        /// <returns>The timer instance</returns>
+        public Timer CreateTimer(int milliseconds, ElapsedEventHandler elapsedEventHandler, bool autoReset, bool enabled)
         {
-            Timer timer = new Timer(miliseconds);
+            Timer timer = new Timer(milliseconds);
             timer.Elapsed += elapsedEventHandler;
             timer.AutoReset = autoReset;
             timer.Enabled = enabled;
+            return timer;
         }
     }
 }
