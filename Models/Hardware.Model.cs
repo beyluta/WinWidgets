7930637bc8ec35c9f352b86d0dc8e7e3a860aad4
Bedirhan Yenilmez <76536654+beyenilmez@users.noreply.github.com>
2023-12-14 21:22:47 +0300

    feat: #58 - add more battery info
    * modified onNativeBatteryLevelEvent
    * updated template battery widget to show charging status and remaining time
    * updated the index.html to show the usage of the new event

--- Models/Hardware.Model.cs
+++ Models/Hardware.Model.cs
@@ -3,7 +3,7 @@
     internal enum HardwareEvent
     {
         NativeKeys = 0,
-        BatteryLevel = 1,
+        Battery = 1,
         SpaceAvailable = 2,
     }
 
@@ -48,5 +48,42 @@
         /// Offset of the mouse wheel. Mouse only
         /// </summary>
         public string mouseWheelOffset { get; set; }
+    }
+
+    internal struct BatteryInfo {
+        /// <summary>
+        /// Battery charging status: Charging, Critical, High, Low, NoSystemBattery, Unknown
+        /// </summary>
+        public string batteryChargeStatus {
+            get; set;
+        }
+
+        /// <summary>
+        /// Full lifetime of the battery in seconds
+        /// </summary>
+        public string batteryFullLifetime {
+            get; set;
+        }
+
+        /// <summary>
+        /// Percentage of the remaining battery life
+        /// </summary>
+        public string batteryLifePercent {
+            get; set;
+        }
+
+        /// <summary>
+        /// Remaining battery lifetime in seconds
+        /// </summary>
+        public string batteryLifeRemaining {
+            get; set;
+        }
+
+        /// <summary>
+        /// Power line status: Offline, Online, Unknown
+        /// </summary>
+        public string powerLineStatus {
+            get; set;
+        }
     }
 }
