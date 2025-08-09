46eabc4e233c2747d8f4bd54cca973b4ef1ed260
Beyluta <beyluta1@gmail.com>
2025-08-09 14:11:15 +0200

    chore: add better comments

--- src/widget_windows.c
+++ src/widget_windows.c
@@ -2,37 +2,47 @@
 #include "widget.h"
 #include <minwindef.h>
 #include <stdio.h>
+#include <time.h>
 #include <windows.h>
 #include <winscard.h>
 #include <WebView2.h>
 
 constexpr char CLASS_NAME[] = "WidgetClass";
 
-static HWND hWnd = nullptr;
+// ----------------------------------------------------------
+// Global variables to control the state of the main window |
+// ----------------------------------------------------------
+static HWND hWndParent = nullptr;
 static ICoreWebView2Controller *webviewController = nullptr;
 static ICoreWebView2 *webviewWindow = nullptr;
 static bool bEnvCreated = false;
 static ULONG HandlerRefCount = 0;
 
+// ----------------------------------------------------------------
+// Global variables to control the state of current opened widget |
+// ----------------------------------------------------------------
+static char tmplName[BUFFSIZE] = {};
+static char tmplPath[BUFFSIZE] = {};
+
+// --------------------------------------------------
+// Vtable functions for creating the default window |
+// --------------------------------------------------
 static ULONG HandlerAddRef(IUnknown *) { return ++HandlerRefCount; }
-
 static ULONG HandlerRelease(IUnknown *) { return --HandlerRefCount; }
-
 static HRESULT HandlerQueryInterface(IUnknown *this, const IID *riid,
                                      void **ppvObject) {
   *ppvObject = this;
   HandlerAddRef(this);
   return EXIT_REASON_TERMINATED;
 }
+
 static HRESULT HandlerInvoke(IUnknown *this, HRESULT errorCode,
                              ICoreWebView2Controller *arg);
-
 ICoreWebView2CreateCoreWebView2ControllerCompletedHandlerVtbl
     completedHandlerVtbl = {.AddRef = (void *)HandlerAddRef,
                             .Release = (void *)HandlerRelease,
                             .QueryInterface = (void *)HandlerQueryInterface,
                             .Invoke = (void *)HandlerInvoke};
-
 ICoreWebView2CreateCoreWebView2ControllerCompletedHandler completedHandler = {
     .lpVtbl = &completedHandlerVtbl};
 
@@ -42,7 +52,8 @@ static HRESULT HandlerInvoke(IUnknown *this, HRESULT errorCode,
     bEnvCreated = true;
 
     ICoreWebView2Environment *env = (ICoreWebView2Environment *)arg;
-    env->lpVtbl->CreateCoreWebView2Controller(env, hWnd, &completedHandler);
+    env->lpVtbl->CreateCoreWebView2Controller(env, hWndParent,
+                                              &completedHandler);
     return EXIT_REASON_TERMINATED;
   }
 
@@ -64,12 +75,16 @@ static HRESULT HandlerInvoke(IUnknown *this, HRESULT errorCode,
   settings->lpVtbl->put_IsStatusBarEnabled(settings, true);
 
   RECT bounds;
-  GetClientRect(hWnd, &bounds);
+  GetClientRect(hWndParent, &bounds);
   webviewController->lpVtbl->put_Bounds(webviewController, bounds);
   webviewWindow->lpVtbl->Navigate(webviewWindow, L"https://google.com");
   return EXIT_REASON_TERMINATED;
 }
 
+// ---------------------------------------------------------------------
+// Callback function used by the window cration process of the default |
+// windows api to process messages.                                    |
+// ---------------------------------------------------------------------
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam,
                             LPARAM lParam) {
   switch (uMsg) {
@@ -79,7 +94,7 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam,
   case WM_SIZE:
     if (webviewController != nullptr) {
       RECT bounds;
-      GetClientRect(hWnd, &bounds);
+      GetClientRect(hWndParent, &bounds);
       webviewController->lpVtbl->put_Bounds(webviewController, bounds);
     }
     return EXIT_REASON_TERMINATED;
@@ -87,6 +102,9 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam,
   return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
 
+// -----------------------------------------------------
+// Event loop of the application.                      |
+// -----------------------------------------------------
 static bool event_loop() {
   MSG msg;
   BOOL bRet;
@@ -102,15 +120,22 @@ static bool event_loop() {
   return false;
 }
 
+// --------------------------------------------------------------------------
+// Here we are linking the previously declared functions to the pointers in |
+// the vtables.                                                             |
+// --------------------------------------------------------------------------
 ICoreWebView2CreateCoreWebView2EnvironmentCompletedHandlerVtbl envHandlerVtbl =
     {.AddRef = (void *)HandlerAddRef,
      .Release = (void *)HandlerRelease,
      .QueryInterface = (void *)HandlerQueryInterface,
      .Invoke = (void *)HandlerInvoke};
-
 ICoreWebView2CreateCoreWebView2EnvironmentCompletedHandler envHandler = {
     .lpVtbl = &envHandlerVtbl};
 
+// --------------------------------------------------------------------------
+// Initializing the main window of the application. It will be responsible  |
+// for spawning children widgets.                                           |
+// --------------------------------------------------------------------------
 bool ww_init_main(HINSTANCE hInstance, int nCmdShow, ww_window_ctx *context,
                   ww_widget_ctx *widgets) {
   if (FAILED(CoInitialize(NULL))) {
@@ -118,15 +143,25 @@ bool ww_init_main(HINSTANCE hInstance, int nCmdShow, ww_window_ctx *context,
     return true;
   }
 
-  char WINDOW_NAME[BUFFSIZE];
-  const size_t len = strlen(context->title);
+  // Copying the title of the window to a local variable here
+  size_t len = strlen(context->title);
   if (len >= BUFFSIZE) {
     fprintf(stderr, "Window name was bigger than expectd\n");
     CoUninitialize();
     return true;
   }
-  memcpy(WINDOW_NAME, context->title, len);
-  WINDOW_NAME[len] = '\0';
+  memcpy(tmplName, context->title, len);
+  tmplName[len] = '\0';
+
+  // Copying the complete path to the wiget in the machine
+  len = strlen(context->filename);
+  if (len >= BUFFSIZE) {
+    fprintf(stderr, "Path to the specified file location was too big\n");
+    return true;
+  }
+  memcpy(tmplPath, context->filename, len);
+  tmplPath[len] = '\0';
+  printf("Path is: %s\n", tmplPath);
 
   WNDCLASS wc = {};
   wc.lpfnWndProc = WindowProc;
@@ -135,26 +170,27 @@ bool ww_init_main(HINSTANCE hInstance, int nCmdShow, ww_window_ctx *context,
 
   RegisterClass(&wc);
 
-  hWnd =
-      CreateWindowEx(0,                   // Optional window styles.
-                     CLASS_NAME,          // Window class
-                     WINDOW_NAME,         // Window text
-                     WS_OVERLAPPEDWINDOW, // Window style
-                     // Size and position
-                     CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
-                     NULL,      // Parent window
-                     NULL,      // Menu
-                     hInstance, // Instance handle
-                     NULL       // Additional application data
-      );
-  if (hWnd == nullptr) {
+  hWndParent = CreateWindowEx(0,                   // Optional window styles.
+                              CLASS_NAME,          // Window class
+                              tmplName,            // Window text
+                              WS_OVERLAPPEDWINDOW, // Window style
+                              CW_USEDEFAULT,       // X Position
+                              CW_USEDEFAULT,       // Y Position
+                              CW_USEDEFAULT,       // Width
+                              CW_USEDEFAULT,       // Height
+                              NULL,                // Parent window
+                              NULL,                // Menu
+                              hInstance,           // Instance handle
+                              NULL // Additional application data
+  );
+  if (hWndParent == nullptr) {
     fprintf(stderr, "Failed to create window\n");
     CoUninitialize();
     return true;
   }
 
-  ShowWindow(hWnd, nCmdShow);
-  UpdateWindow(hWnd);
+  ShowWindow(hWndParent, nCmdShow);
+  UpdateWindow(hWndParent);
 
   CreateCoreWebView2EnvironmentWithOptions(nullptr, nullptr, nullptr,
                                            &envHandler);
